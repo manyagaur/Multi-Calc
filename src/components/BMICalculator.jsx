25b8baf24a155e0a8c071000998910edb4125a99
manyagaur <manyagaur25@gmail.com>
2024-05-26 21:52:31 +0530

    added slider

--- src/components/BMICalculator.jsx
+++ src/components/BMICalculator.jsx
@@ -3,14 +3,26 @@ import React from "react";
 function BMICalculator(){
     return(
         <>
-        <label>Name: </label>
+        <div>
+        <label>Age: </label><br></br>
         <input type="text"></input><br></br><br></br>
-        <label>Age: </label>
-        <input type="text"></input><br></br><br></br>
-        <label>Gender: </label>
+        <label>Gender: </label><br></br>
         Male<input type="radio"></input>
         Female<input type="radio"></input><br></br><br></br>
-        
+        <label>Height: </label><br></br>
+        <input type="number" placeholder="(in cm)"></input><br></br><br></br>
+        <label>Weight: </label><br></br>
+        <input type="number" placeholder="(in kg)"></input>
+        </div><br></br>
+        <div className="bmi-chart">
+            <div className="bmi-indicator">
+            <div className="yellow"></div>
+            <div className="green"></div>
+            <div className="orange"></div>
+            <div className="red"></div>
+            </div>
+        </div>
+        <input type="range" className="slider-class" min="0" max="18" value="50"></input>
         </>
     )
 }
